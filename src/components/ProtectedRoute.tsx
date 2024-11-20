073f7d51f6c81e151551c029783b36a101ce3495
SouleymaneSy7 <souleymanesyservices@gmail.com>
2024-11-20 19:33:55 +0000

    feat: update the protected route handler

--- src/components/ProtectedRoute.tsx
+++ src/components/ProtectedRoute.tsx
@@ -1,13 +1,18 @@
 import React from "react";
 import { Navigate, Outlet } from "react-router-dom";
+import { useAuthContext } from "../contexts/AuthContext";
 
-const ProtectedRoute: React.FC = () => {
-  const user = null;
+const ProtectedRoute: React.FC<{ children: React.ReactNode }> = ({
+  children,
+}) => {
+  const { isAuthenticated, loading } = useAuthContext();
 
-  return user ? (
-    <React.Fragment>
-      <Outlet />
-    </React.Fragment>
+  if (loading) {
+    return <div>Loading...</div>;
+  }
+
+  return isAuthenticated ? (
+    <React.Fragment>{children}</React.Fragment>
   ) : (
     <React.Fragment>
       <Navigate to="/" />
