60be86777ea961886bcdc61706b8284ae600022e
SouleymaneSy7 <souleymanesyservices@gmail.com>
2024-11-20 19:32:14 +0000

    fix: fix the appwrite configuration

--- src/configs/appwriteConfig.ts
+++ src/configs/appwriteConfig.ts
@@ -2,8 +2,8 @@ import { Client, Account } from "appwrite";
 import { APPWRITE_ENDPOINT_URL, APPWRITE_PROJECT_ID } from "./constants";
 
 const client = new Client()
-  .setEndpoint(APPWRITE_ENDPOINT_URL)
-  .setProject(APPWRITE_PROJECT_ID);
+  .setEndpoint(`${APPWRITE_ENDPOINT_URL}`)
+  .setProject(`${APPWRITE_PROJECT_ID}`);
 
 const account = new Account(client);
 
