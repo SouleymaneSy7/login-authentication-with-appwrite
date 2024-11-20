50c0cca22b8eb665646bed607cbdac915c1fb534
SouleymaneSy7 <souleymanesyservices@gmail.com>
2024-11-20 14:22:44 +0000

    config: add the appwrite configuration

---
+++ src/configs/appwriteConfig.ts
@@ -0,0 +1,10 @@
+import { Client, Account } from "appwrite";
+import { APPWRITE_ENDPOINT_URL, APPWRITE_PROJECT_ID } from "./constants";
+
+const client = new Client()
+  .setEndpoint(APPWRITE_ENDPOINT_URL)
+  .setProject(APPWRITE_PROJECT_ID);
+
+const account = new Account(client);
+
+export { client, account };
---
+++ src/configs/constants.ts
@@ -0,0 +1,4 @@
+export const APPWRITE_ENDPOINT_URL: string = import.meta.env
+  .VITE_APPWRITE_ENDPOINT_URL;
+export const APPWRITE_PROJECT_ID: string = import.meta.env
+  .VITE_APPWRITE_PROJECT_ID;
