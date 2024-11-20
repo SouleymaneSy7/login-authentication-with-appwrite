cd7d732498517717af90b3108b7beb91a3b10cc6
SouleymaneSy7 <souleymanesyservices@gmail.com>
2024-11-20 15:17:20 +0000

    chore: update the types

--- src/types/index.ts
+++ src/types/index.ts
@@ -1,3 +1,5 @@
+import { Models } from "appwrite";
+
 export type ButtonPropsType = {
   children: React.ReactNode;
   type: "button" | "submit";
@@ -27,12 +29,29 @@ export interface FormInputPropsType {
   errors?: string | undefined;
 }
 
-export interface SuccessPagePropsType {
-  user: { name: string } | null;
-  signOut: () => Promise<void>;
-}
-
 export interface ProviderButtonPropsType {
   children: React.ReactNode;
   providerName: "google" | "github" | "discord";
+}
+
+export interface User extends Models.User<Models.Preferences> {}
+
+export interface SuccessPagePropsType {
+  user: User | null;
+  signOut: () => Promise<void>;
+}
+
+export interface AuthContextType {
+  user: User | null;
+  loading: boolean;
+  register: (
+    email: string,
+    password: string,
+    full_name: string
+  ) => Promise<void>;
+  logIn: (
+    email: string,
+    password: string
+  ) => Promise<Models.Session | undefined>;
+  signOut: () => Promise<void>;
 }
