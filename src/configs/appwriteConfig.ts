ac00a083706066da7a003a1acfbf27d05dc1e9c0
SouleymaneSy7 <souleymanesyservices@gmail.com>
2024-11-21 21:19:38 +0000

    fix: fix the appwrite configs errors

--- src/configs/appwriteConfig.ts
+++ src/configs/appwriteConfig.ts
@@ -1,9 +1,8 @@
 import { Client, Account } from "appwrite";
-import { APPWRITE_ENDPOINT_URL, APPWRITE_PROJECT_ID } from "./constants";
 
 const client = new Client()
-  .setEndpoint(`${APPWRITE_ENDPOINT_URL}`)
-  .setProject(`${APPWRITE_PROJECT_ID}`);
+  .setEndpoint(import.meta.env.VITE_APPWRITE_ENDPOINT_URL)
+  .setProject(import.meta.env.VITE_APPWRITE_PROJECT_ID);
 
 const account = new Account(client);
 
--- src/configs/constants.ts
+++
@@ -1,4 +0,0 @@
-export const APPWRITE_ENDPOINT_URL: string = import.meta.env
-  .VITE_APPWRITE_ENDPOINT_URL;
-export const APPWRITE_PROJECT_ID: string = import.meta.env
-  .VITE_APPWRITE_PROJECT_ID;
