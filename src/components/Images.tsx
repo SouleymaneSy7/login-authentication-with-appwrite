12d956e3d052846443211d90e58e4c1f136c45e1
SouleymaneSy7 <souleymanesyservices@gmail.com>
2024-11-22 23:17:53 +0000

    feat: update the videos component to be image component

---
+++ src/components/Images.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import getRandomContent from "../utils/getRandomContents";
+
+const Images: React.FC = () => {
+  const imagesList = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "10"];
+
+  return (
+    <section className="w-full max-w-[448px] mx-auto h-[210px] rounded-border-radius-sm mb-7 overflow-hidden md:max-w-[530px] md:h-[280px] lg:h-full lg:w-1/2  lg:mb-0 lg:rounded-none lg:max-w-none ">
+      <img src={`/src/assets/images/${getRandomContent(imagesList)}.jpg`} />
+    </section>
+  );
+};
+
+export default Images;
