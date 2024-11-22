88b59920182e6e2c12fce75b04fe7e3576976b62
SouleymaneSy7 <souleymanesyservices@gmail.com>
2024-11-22 23:16:58 +0000

    feat: update the App component

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,11 @@
 import React from "react";
 
-// import Videos from "./components/Videos";
 import RouterConfig from "./routes/RoutesConfig";
 import { AuthProvider } from "./contexts/AuthContext";
 
 const App: React.FC = () => {
   return (
     <AuthProvider>
-      {/* <Videos /> */}
       <RouterConfig />
     </AuthProvider>
   );
