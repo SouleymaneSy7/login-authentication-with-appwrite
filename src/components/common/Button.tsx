60c922b2147b44d23daae5bf450b093c622eaf4d
SouleymaneSy7 <souleymanesyservices@gmail.com>
2024-11-22 23:15:05 +0000

    feat: update the buttons styling

--- src/components/common/Button.tsx
+++ src/components/common/Button.tsx
@@ -7,7 +7,11 @@ const Button: React.FC<ButtonPropsType> = ({
   ...delegatedProps
 }) => {
   return (
-    <button type={type} {...delegatedProps}>
+    <button
+      type={type}
+      className="w-full h-[42px] rounded-border-radius-sm bg-dark-bg-clr text-white-text-clr font-fw-semi-bold mt-10 text-fs-body tracking-tighter lg:h-[52px]"
+      {...delegatedProps}
+    >
       {children}
     </button>
   );
