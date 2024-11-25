154cc3ddbfcc56a17f8913057b7c23ffdbbe57f8
Souleymane Sy <110754587+SouleymaneSy7@users.noreply.github.com>
2024-11-25 15:01:10 +0000

    fix: fix the images component src path errors

--- src/components/Images.tsx
+++ src/components/Images.tsx
@@ -6,7 +6,7 @@ const Images: React.FC = () => {
 
   return (
     <section className="w-full max-w-[448px] mx-auto h-[210px] rounded-border-radius-sm mb-7 overflow-hidden md:max-w-[530px] md:h-[280px] lg:h-full lg:w-1/2  lg:mb-0 lg:rounded-none lg:max-w-none ">
-      <img src={`/src/assets/images/${getRandomContent(imagesList)}.jpg`} />
+      <img src={`../assets/images/${getRandomContent(imagesList)}.jpg`} alt="abstracts 3d images" />
     </section>
   );
 };
